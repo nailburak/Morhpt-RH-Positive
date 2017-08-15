2797146a9e54bfa4d9a0a76602279a2b1a066177
Sait Yalcin <saityalcin@Brunos-MacBook-Pro.local>
2017-08-15 12:30:20 -0400

    router added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { CunsumerComponent } from './cunsumer/cunsumer.component';
 import { AdminComponent } from './admin/admin.component';
 import { CHomeComponent } from './c-home/c-home.component';
 import { AHomeComponent } from './a-home/a-home.component';
+
+const appRoutes: Routes = [
+  { path: '', component: CunsumerComponent, 
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: CHomeComponent },
+    ]
+  },
+  { path: 'admin', component: AdminComponent,
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: AHomeComponent },
+    ]
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
