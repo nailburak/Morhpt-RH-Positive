7973524c69c9e8056323bed6435245c16bc1a1cd
Sait Yalcin <saityalcin@Brunos-MacBook-Pro.local>
2017-08-15 12:26:40 -0400

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CunsumerComponent } from './cunsumer/cunsumer.component';
+import { AdminComponent } from './admin/admin.component';
+import { CHomeComponent } from './c-home/c-home.component';
+import { AHomeComponent } from './a-home/a-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CunsumerComponent,
+    AdminComponent,
+    CHomeComponent,
+    AHomeComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/cunsumer/cunsumer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cunsumer',
+  templateUrl: './cunsumer.component.html',
+  styleUrls: ['./cunsumer.component.css']
+})
+export class CunsumerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
